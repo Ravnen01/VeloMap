25ce3f4a9cc6eb0b5fdc22655d7a882829504a09
Ravnen01 <lecomte.corentin@gmail.com>
2015-10-13 23:32:42 +0200

    Fin de travail

--- VeloMap/app/src/main/java/lpiem/lecomte/com/velomap/JCDecaux.java
+++ VeloMap/app/src/main/java/lpiem/lecomte/com/velomap/JCDecaux.java
@@ -1,7 +1,37 @@
 package lpiem.lecomte.com.velomap;
 
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
 /**
  * Created by LECOMTE on 13/10/2015.
  */
 public class JCDecaux {
+
+    private static String apikey="626f84cb00f2d7868af5b65d34317c54c2158acd";
+
+    private static JCDecaux ourInstance = new JCDecaux();
+
+    public static JCDecaux getInstance() {
+        return ourInstance;
+    }
+
+    private JCDecaux() {
+    }
+
+    public JSONArray listJCDContract(){
+
+        DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+        HttpPost httppost = new HttpPost("http://someJSONUrl/jsonWebService");
+
+        httppost.setHeader("Content-type", "application/json");
+
+
+
+    }
+
 }
